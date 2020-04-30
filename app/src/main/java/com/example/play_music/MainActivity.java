aca07a870353f16bf6047c1e2b8a0b27f891701c
aziz <19atabaev.aziz@gmail.com>
2020-04-30 10:47:34 +0500

    Play music apk Volume is installed Project is fixed

--- app/src/main/java/com/example/play_music/MainActivity.java
+++ app/src/main/java/com/example/play_music/MainActivity.java
@@ -1,20 +1,49 @@
 package com.example.play_music;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     ImageView playPauseIcon;
+    SeekBar volumeSeekBar;
+    AudioManager audioManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("Progress changed: ", "" + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         playPauseIcon = findViewById(R.id.playPauseIcon);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.wewillrockyou);
